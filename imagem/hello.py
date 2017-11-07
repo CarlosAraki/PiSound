7539148299a50d66af2da72cf31dec89437a9be1
Carlos Araki <carlosarak@gmail.com>
2017-11-07 17:38:18 -0200

    agora da grey e criei uma pasta som

--- imagem/hello.py
+++ imagem/hello.py
@@ -3,10 +3,9 @@ from SimpleCV import *
 
 cam = Camera(0,{"width":640 , "height":480})
 
-#while True:
-img = cam.getImage()
-
-gray =  img.grayscale()
-print gray.getPixel(0,0)
-gray.show()
-time.sleep(10)
+while True:
+    img = cam.getImage()
+    gray =  img.grayscale()
+    print gray.getPixel(0,0)
+    gray.show()
+    time.sleep(3)
