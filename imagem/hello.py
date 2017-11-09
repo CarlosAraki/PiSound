4d3ddb0733e54d650e171a6e2d37bb02ab0c024b
Carlos Araki <carlosarak@gmail.com>
2017-11-09 17:58:18 -0200

    agora com abertura de arquivos

--- imagem/hello.py
+++ imagem/hello.py
@@ -1,11 +1,17 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from SimpleCV import *
 
 cam = Camera(0,{"width":640 , "height":480})
 
 while True:
+    arq = open('fifo.txt', 'w')
     img = cam.getImage()
     gray =  img.grayscale()
-    print gray.getPixel(0,0)
     gray.show()
+
+    x = 1
+    y = 6
+    arq.write("%f %f"%(x,y))
+    arq.close()
     time.sleep(3)
