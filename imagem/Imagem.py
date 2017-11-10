3e1e5e6b8a24def38e3aed57c01e48cbfe0703bc
CarlosAraki <carlosarak@gmail.com>
2017-11-10 17:11:26 +0000

    melhorias

--- imagem/Imagem.py
+++ imagem/Imagem.py
@@ -1,22 +1,35 @@
+
 from SimpleCV import *
 
 cam = Camera(0,{"width":640 , "height":480}) #307.200 pixels
 
-while True:
-    k= 0
-    arq = open('fifo.txt', 'w')
-    img = cam.getImage()
+#while True:
+k= 0
+arq = open('fifo.txt', 'w')
+img = cam.getImage()
+contx = 0
+conty = 0
+a = 0
+i = 0
+while i != 640:
+	j= 0
+	while j !=480:
+		color =  img.getPixel(i,j) #pego pixel
+		#if color[1] > 100 and color[0] < 50 and color[2] < 50:
+		conty = conty +j
+		contx = contx+i
+		a = a+1
+		j = j+1
+	i = i+1
+# 1 -> 100% 16seg
+#2 -> 100% 10 seg
+# 4 -> 99% 8.69 seg 
+# 8 -> 99% 8.29 seg		
+if a == 0:
+	a = 1
 
-
-    for i = 0 to 639
-        for j = 0 to 479
-            color =  img.getPixel(i,j) #pego pixel
-            if color[1] > 200 and color[0] < 50 and color[2] < 50
-                pos[k]=  (i,j)
-    #gray.show()
-
-    x = 1
-    y = 6
-    arq.write("%f %f"%(x,y))
-    arq.close()
-    time.sleep(3)
+x = contx/a
+y = conty/a
+arq.write("%f %f"%(x,y))
+arq.close()
+#time.sleep(3)
