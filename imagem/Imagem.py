b68e04e24c8d0d1494458a1c2bdee7b406fcf8fc
Unknown <carlosarak@gmail.com>
2017-11-14 16:54:34 -0200

    voltando para original

--- imagem/Imagem.py
+++ imagem/Imagem.py
@@ -9,7 +9,7 @@ altura = 120						#altura  do frame
 cam = Camera(0,{"width":largura ,"height":altura}) 	#original  {"width":640 , "height":480}) #307.200 pixels
 nome_arq = sys.argv					#nome do arquivo
 							#abrir gravar e fechar arquivo demora 7xE-4s
-print 4
+
 while True:						#loop de frame a frame
 	comeco = time.time()				#tempo inicial
 	img = cam.getImage()				#capturo o frame
